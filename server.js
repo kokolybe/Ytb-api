9eefae5d8d5a4a59604c617956686b16ef9f86e3
kokolybe <52601492+kokolybe@users.noreply.github.com>
2024-12-07 07:54:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const https = require('https');
+const request = require('request');
 const app = express();
 
 // Port défini par Render ou par défaut 3000
@@ -42,6 +43,33 @@ app.get('/search', (req, res) => {
     });
 
     apiReq.end();
+});
+
+app.get('/download', (req, res) => {
+    const videoId = req.query.id;
+
+    if (!videoId) {
+        return res.status(400).send({ error: 'Le paramètre "id" est requis.' });
+    }
+
+    const options = {
+        method: 'GET',
+        url: 'https://ytstream-download-youtube-videos.p.rapidapi.com/dl',
+        qs: { id: videoId },
+        headers: {
+            'x-rapidapi-key': '36f745d486mshd6d8e5421dd8280p1f06d0jsn7a4caec6be1c',
+            'x-rapidapi-host': 'ytstream-download-youtube-videos.p.rapidapi.com',
+        },
+    };
+
+    request(options, (error, response, body) => {
+        if (error) {
+            console.error(error);
+            return res.status(500).send({ error: 'Une erreur est survenue lors de l\'appel à l\'API.' });
+        }
+
+        res.status(200).send(JSON.parse(body));
+    });
 });
 
 app.listen(PORT, () => {
